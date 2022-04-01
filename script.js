206e4943eb468476ee52f5d795af44f3f0115f42
chillzeus <fiedlerisaac1@gmail.com>
2022-04-01 14:47:04 -0500

    json stuff

---
+++ script.js
@@ -0,0 +1,13 @@
+let teamdata = [
+    {
+    team: "Fiedlers",
+    isaac: [
+        {
+            city: "Greenfield",
+            age: 14
+        }
+    ]
+    }
+];
+
+console.log(teamdata[0].data[0].city)
