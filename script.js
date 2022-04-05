1e6e05e36aeb7b8a4491ff76d59e7a7bec207bb4
chillzeus <fiedlerisaac1@gmail.com>
2022-04-05 14:53:31 -0500

    Pre Alpha 0.0.1
    I realized this is not a pre release, it is a pre alpha. my apologies

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ let player1 = [
     name: "Isaac Fiedler",
     handed: "right",
     battingrating: 1.2,
-    strength: 1.5
+    strength: 1.5,
+    hits: 0
 	}
 ]
 
@@ -17,7 +18,9 @@ let pitcher = [
     name: "Bizarro Isaac",
     handed: "left",
     pitchingrating: 1.4,
-    pitchspeed: 1.7
+    pitchspeed: 1.7,
+    exhaustion: 1,
+    endurance: 55
   }
 ]
 
@@ -35,33 +38,43 @@ function atBat() {
   //sees who is the "better player"
   if(pitcher[0].pitchingrating > player1[0].battingrating) {
     probability += 5;
-  } 
+  }
   if(pitcher[0].pitchingrating < player1[0].battingrating) {
     probability += -2;
   }
   if(pitcher[0].handed !== player1[0].handed) {
-    probability += 1;
+    probability += -1;
+  } if (pitcher[0].exhaustion == 2) {
+    probability += -1;
+  } if (pitcher[0].exhaustion == 3) {
+    probability += -2;
   }
 
-  //does the probability
-  randomNumber = Math.trunc(Math.random() * (probability - 0) + 0);
-  if (randomNumber == 1 || randomNumber == 2 || randomNumber == 3) {
-    console.log("hit")
-    hit = true;
-  } 
-  if (randomNumber == 4 || randomNumber == 5 || randomNumber == 6) {
-    console.log("ball")
-  } 
-  if (randomNumber == 7) {
-    if (player1[0].strength >= 1.5) {
-      homeRun = 2;
+  // if strikes and stuff
+  if (strikes < 3 || balls < 4) { 
+    randomNumber = Math.trunc(Math.random() * (probability - 0) + 0);
+    if (randomNumber == 1 || randomNumber == 2 || randomNumber == 3) {
+      console.log("hit")
+      hit = true;
     } 
-    else {
-      homeRun = 4;
+    if (randomNumber == 4 || randomNumber == 5 || randomNumber == 6) {
+      console.log("ball")
+    } 
+    //home run calculations
+    if (randomNumber == 7) {
+      if (player1[0].strength >= 1.5) {
+        homeRun = 2;
+      } 
+      else {
+        homeRun = 4;
+      }
+      homeRun = Math.trunc(Math.random() * (homeRunProbability - 0) + 0); 
+      if (homeRun == 1) {
+        console.log("home run")
+      }
     }
-    homeRun = Math.trunc(Math.random() * (homeRunProbability - 0) + 0); 
-    if (homeRun == 1) {
-      console.log("home run")
+    if (randomNumber > 7) {
+      console.log("strike")
     }
   }
 }
