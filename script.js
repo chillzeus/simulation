f0a1b571ac1c0523eda911f4d5cf39f9a732cd63
chillzeus <fiedlerisaac1@gmail.com>
2022-04-05 14:34:15 -0500

    0.0.0 Pre Release
    Has some basic probability stuff added, also calculates "skill" of a player.

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let player1 = [
     name: "Isaac Fiedler",
     handed: "right",
     battingrating: 1.2,
+    strength: 1.5
 	}
 ]
 
@@ -15,13 +16,17 @@ let pitcher = [
   {
     name: "Bizarro Isaac",
     handed: "left",
-    pitchingrating: 1.4
+    pitchingrating: 1.4,
+    pitchspeed: 1.7
   }
 ]
 
 //will need a hit is true variable
 let probability = 12;
+let homeRunProbability = null;
+
 let randomNumber = null;
+let homeRun = null;
 let hit = false;
 
 function atBat() {
@@ -34,17 +39,32 @@ function atBat() {
   if(pitcher[0].pitchingrating < player1[0].battingrating) {
     probability += -2;
   }
+  if(pitcher[0].handed !== player1[0].handed) {
+    probability += 1;
+  }
 
   //does the probability
   randomNumber = Math.trunc(Math.random() * (probability - 0) + 0);
   if (randomNumber == 1 || randomNumber == 2 || randomNumber == 3) {
     console.log("hit")
     hit = true;
-  } if (randomNumber == 4 || randomNumber == 5 || randomNumber == 6) {
+  } 
+  if (randomNumber == 4 || randomNumber == 5 || randomNumber == 6) {
     console.log("ball")
-  } if (randomNumber == 7) {
-    //
+  } 
+  if (randomNumber == 7) {
+    if (player1[0].strength >= 1.5) {
+      homeRun = 2;
+    } 
+    else {
+      homeRun = 4;
+    }
+    homeRun = Math.trunc(Math.random() * (homeRunProbability - 0) + 0); 
+    if (homeRun == 1) {
+      console.log("home run")
+    }
   }
 }
 
 atBat()
+//for testing
