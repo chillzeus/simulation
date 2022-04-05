012980e259f1b0e2c48562b5f0be9a27d6403e79
chillzeus <76137066+chillzeus@users.noreply.github.com>
2022-04-05 11:55:40 -0500

    Changed sport, worked on probability

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
-let greenfield = [
-	{
-	team: "Greenfield",
-  wins: 0,
-  losses: 0,
-  players: [
-  	{
-  	  player1: [
-        {
-          name: "Isaac",
-        }],
-      player2: [
-        {
-          name: "Soren",
-        }],
-    }]
+//an all new BASEBALL SIMULATOR because it's easier than basketball
+
+let balls = 0;
+let strikes = 0;
+
+let player1 = [
+	{        
+    name: "Isaac Fiedler",
+    handed: "right",
+    battingrating: 1.2,
 	}
 ]
 
-let gameTimeRAW = 192;
-let score = gameTimeRAW / 4;
+let pitcher = [
+  {
+    name: "Bizarro Isaac",
+    handed: "left",
+    pitchingrating: 1.4
+  }
+]
 
-//this is where one play will be
-//algorithm needed, defense will be man to man lol, zone would be hard to code
-function play() {
+//will need a hit is true variable
+let probability = 12;
+let randomNumber = null;
+let hit = false;
 
+function atBat() {
+  //enclose this all in a for loop or something
+
+  //sees who is the "better player"
+  if(pitcher[0].pitchingrating > player1[0].battingrating) {
+    probability += 5;
+  } 
+  if(pitcher[0].pitchingrating < player1[0].battingrating) {
+    probability += -2;
+  }
+
+  //does the probability
+  randomNumber = Math.trunc(Math.random() * (probability - 0) + 0);
+  if (randomNumber == 1 || randomNumber == 2 || randomNumber == 3) {
+    console.log("hit")
+    hit = true;
+  } if (randomNumber == 4 || randomNumber == 5 || randomNumber == 6) {
+    console.log("ball")
+  } if (randomNumber == 7) {
+    //
+  }
 }
 
-//increment by quarter minute?
-for (let i = 192; i > 0; i += -1) {
-  play()
-}
-
-console.log(greenfield[0].players[0].name)
+atBat()
