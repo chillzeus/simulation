a1b013145106c6423d0741dd0b5a23b65563fa50
chillzeus <76137066+chillzeus@users.noreply.github.com>
2022-04-08 13:50:24 -0500

    0.0.4 Pre Alpha (basically doesn't work)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 0.0.2 Pre Alpha
+// 0.0.4 Pre Alpha
 
 let balls = 0;
 let strikes = 0;
@@ -12,6 +12,7 @@ let hitType = 100;
 
 let randomNumber = null;
 let randomHit = null;
+let secondary = null;
 let inPlay = false;
 
 //player[i].etc USE FOR LOOP!
@@ -38,67 +39,64 @@ const pitcher = [
 ]
 
 function atBat() {
-  if (balls < 4 && strikes < 3) {
-    //enclose this all in a for loop or something
+  //sees who is the "better player"
+  if(pitcher[0].pitchingrating > player1[0].battingrating) {
+    probability += 5;
+  }
+  if(pitcher[0].pitchingrating < player1[0].battingrating) {
+    probability += -2;
+  }
+  if(pitcher[0].handed !== player1[0].handed) {
+    probability += -1;
+  } if (pitcher[0].exhaustion == 2) {
+    probability += -1;
+  } if (pitcher[0].exhaustion == 3) {
+    probability += -2;
+  }
 
-    //sees who is the "better player"
-    if(pitcher[0].pitchingrating > player1[0].battingrating) {
-      probability += 5;
+  // if strikes and stuff
+  if (strikes < 3 || balls < 4) { 
+    randomNumber = Math.trunc(Math.random() * (probability - 0) + 0);
+    if (randomNumber <= 6) {
+      inPlay = true;
+    } 
+    if (randomNumber >= 7 && randomNumber <= 17) {
+      console.log("ball")
+      balls += 1;
     }
-    if(pitcher[0].pitchingrating < player1[0].battingrating) {
-      probability += -2;
-    }
-    if(pitcher[0].handed !== player1[0].handed) {
-      probability += -1;
-    } if (pitcher[0].exhaustion == 2) {
-      probability += -1;
-    } if (pitcher[0].exhaustion == 3) {
-      probability += -2;
+    if (randomNumber >= 18) {
+      console.log("strike")
+      strikes += 1;
     }
 
-    // if strikes and stuff
-    if (strikes < 3 || balls < 4) { 
-      randomNumber = Math.trunc(Math.random() * (probability - 0) + 0);
-      if (randomNumber <= 6) {
-        console.log("hit")
-        inPlay = true;
-      } 
-      if (randomNumber >= 7 && randomNumber <= 17) {
-        console.log("ball")
-        balls += 1;
+    if (inPlay) {
+      if (player1[0].strength > 1 && player1[0].strength < 1.5) {
+        hitType += -4;
+      } if (player1[0].strength > 1.5) {
+        hitType += -6;
       }
-      if (randomNumber >= 18) {
-        console.log("strike")
-        strikes += 1;
+
+      randomHit = Math.trunc(Math.random() * hitType - 0) + 0;
+
+      if (randomHit <= 30) {
+        secondary = Math.trunc(Math.random() * hitType - 0) + 0;
+        if (secondary <= 60) {
+            console.log("single")
+          } if (secondary > 60 && secondary <= 85) {
+            console.log("double")
+          } if (secondary > 85 && secondary <= 95) {
+            console.log("home run")
+          } if (secondary > 95 && secondary <= 100) {
+            console.log("triple")
+          }
+      if (randomHit > 30) {
+        secondary = Math.trunc(Math.random() * hitType - 0) + 0;
+        if (secondary <= 50) {
+          console.log("ground out")
+        } if (secondary > 50) {
+          console.log("fly out")
+        }
       }
-
-      if (inPlay) {
-        if (player1[0].strength > 1 && player1[0].strenght < 1.5) {
-          hitType += -3;
-        } if (player1[0].strength > 1.5) {
-          hitType += -4;
-        }
-
-        randomHit = Math.trunc(Math.random() * hitType - 0) + 0;
-
-        if (randomHit <= 30) {
-          randomHit = Math.trunc(Math.random() * hitType - 0) + 0;
-          if (randomHit <= 60) {
-              console.log("single")
-            } if (randomHit > 60 && randomHit <= 80) {
-              console.log("double")
-            } if (randomHit == 9) {
-              console.log("home run")
-            } if (randomHit == 10 || randomHit == 11) {
-              console.log("double")
-            } if (randomHit == 12) {
-              console.log("triple")
-            } if (randomHit == 13) {
-              console.log("home run")
-            } if (randomHit > 13) {
-              console.log("ground out")
-            }
-        }
       }
     }
   }
